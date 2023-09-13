b4161e2bfea2bb535adbaf05fd2d93ee6587ce96
chuduchung <chuduchung1997@gmail.com>
2023-09-13 15:14:06 +0700

    dientich

---
+++ dientich.js
@@ -0,0 +1,6 @@
+let inputWidth = +prompt("Nhập chiều rộng:");
+let inputHeigh = +prompt("Nhập chiều cao:");
+let result = inputHeigh * inputWidth;
+document.write(result);
+alert(result);
+console.log(result);
